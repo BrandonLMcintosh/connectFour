cd4081e135267aa37dc9058f2a7ed3043e17801c
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-16 17:23:07 -0700

    building tests for all functions

---
+++ connect4.test.js
@@ -0,0 +1,32 @@
+describe('CheckForWin()', () => {
+    
+    it('should identify a horizontal win', () => {
+    
+    })
+
+    it('should identify a vertical win', () => {
+    
+    })
+
+    it('should identify a diagonal down-right win', () => {
+    
+    })
+
+    it('should identify a diagonal down-left win', () => {
+    
+    })
+
+    it('should return false for a non-win', () => {
+    
+    })
+})
+
+describe('checkForTie()', () => {
+    it('should return false for a non-tie', () => {
+
+    })
+
+    it('should return true for a tie', () => {
+        
+    })
+})
