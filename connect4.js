f983a545aac80c40880730e96b94431b0d7c8f5c
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-17 13:30:06 -0700

    finished with testing. 26 specs, 0 failures.

--- connect4.js
+++ connect4.js
@@ -1,11 +1,11 @@
-const WIDTH = 7;
-const HEIGHT = 6;
+let WIDTH = 7;
+let HEIGHT = 6;
 
 let moves = 0;
 let currPlayer = 1;
 //I had to .map the array and sub-arrays because if I left it as "new Array(HEIGHT).fill(new Array(WIDTH).fill(null))" all of the sub-arrays were referencing the same point in memory.
 //.map helps me create a new array with new memory addresses for each subarray. 
-const board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y)); // array of rows, each row is array of cells  (board[y][x])
+let board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y)); // array of rows, each row is array of cells  (board[y][x])
 const handleClick = evt => {
   // get x from ID of clicked cell
   const x = evt.target.id;
@@ -21,9 +21,9 @@ const handleClick = evt => {
   // TODO: add line to update in-memory board
   placeInTable(y, x);
 
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+  // check for win  
+  if (checkForWin()){
+    endGame(`Player ${currPlayer} won!`);
   }
 
   // check for tie
@@ -39,6 +39,8 @@ const handleClick = evt => {
 const makeHtmlBoard = () => {
   //select the board table
   const htmlBoard = document.querySelector('#board');
+  //removing previous cells if there were any
+  htmlBoard.innerHTML = '';
   //create a new row
   const top = document.createElement("tr");
   //set that row id to be column-top which will change the style of the row based on the css
@@ -67,6 +69,7 @@ const makeHtmlBoard = () => {
       //create a new cell for that row
       const cell = document.createElement('td');
       //set the id of that cell to the coordinates that it exists in the table
+      cell.classList.add('cell');
       cell.setAttribute('id', `${yindex}-${xindex}`);
       //append the cell to the row
       row.append(cell);
@@ -95,26 +98,29 @@ const placeInTable = (y, x) => {
 }
 /* endGame: announce game end */
 const endGame = msg => alert(msg);
+const win = cells => {
+  // Check four cells to see if they're all color of current player
+  //  - cells: list of four (y, x) cells
+  //  - returns true if all are legal coordinates & all match currPlayer
+
+  //for every element in the selected cell
+  return cells.every(
+    ([y, x]) =>
+      //If all of the elements are on the board
+      y >= 0 && 
+      y < HEIGHT && 
+      x >= 0 && 
+      x < WIDTH && 
+      board[y][x] === currPlayer
+  )
+}
 /* checkForWin: check board cell-by-cell for "does a win start here?" */
 const checkForWin = () => {
-  const _win = cells => {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
-    //for every element in the selected cell
-    return cells.every(
-      ([y, x]) =>{
-        //If all of the elements are on the board
-        const isWin = ((y >= 0) && (y < HEIGHT) && (x >= 0) && (x < WIDTH) && (board[y][x] === currPlayer));
         //And all of the cells belong to the curent player
-        return isWin;
-      });
-  }
 
   // TODO: read and understand this code. Add comments to help you.
   //For every row
-  board.forEach((row, y) => row.forEach((column, x) => {
+  return board.some((row, y) => row.some((column, x) => {
     //calculate horizontal win (going right) of each cell
     const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
     //calculate vertical win (going up) of each cell
@@ -124,9 +130,13 @@ const checkForWin = () => {
     //calculate diagnonal win going down and to the left of each cell
     const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
     //for each cell, check if any one of the possible win scenarios is true
-    return ((_win(horiz)) || (_win(vert)) || (_win(diagDR)) || (_win(diagDL)));
+    if(win(horiz) || win(vert) || win(diagDR) || win(diagDL)){
+      return true;
+    }
   }));
 }
+
+
 const checkForTie = () => board.every(row => row.every(cell => cell !== null));
 const switchPlayer = () => currPlayer = (moves % 2) + 1;
 makeHtmlBoard();
--- connect4.test.js
+++ connect4.test.js
@@ -1,94 +1,254 @@
+let test;
 describe('CheckForWin()', () => {
     afterEach(() => {
-        board.forEach(val => val.fill(null));
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
     })
     it('should identify a horizontal win', () => {
-        board[5][0] = '1';
-        board[5][1] = '1';
-        board[5][2] = '1';
-        board[5][3] = '1';
+        board[5][0] = 1;
+        board[5][1] = 1;
+        board[5][2] = 1;
+        board[5][3] = 1;
         expect(checkForWin()).toEqual(true);
     })
 
     it('should identify a vertical win', () => {
-        board[5][0] = '1';
-        board[4][0] = '1';
-        board[3][0] = '1';
-        board[2][0] = '1';
+        board[5][0] = 1;
+        board[4][0] = 1;
+        board[3][0] = 1;
+        board[2][0] = 1;
         expect(checkForWin()).toEqual(true);
     })
 
     it('should identify a diagonal down-right win', () => {
-        board[0][0] = '1';
-        board[1][1] = '1';
-        board[2][2] = '1';
-        board[3][3] = '1';
+        board[0][0] = 1;
+        board[1][1] = 1;
+        board[2][2] = 1;
+        board[3][3] = 1;
         expect(checkForWin()).toEqual(true);
     })
 
     it('should identify a diagonal down-left win', () => {
-        board[0][3] = '1';
-        board[1][2] = '1';
-        board[2][1] = '1';
-        board[3][0] = '1';
+        board[0][3] = 1;
+        board[1][2] = 1;
+        board[2][1] = 1;
+        board[3][0] = 1;
         expect(checkForWin()).toEqual(true);
     })
 
     it('should return false for a non-win', () => {
-        board[5][0] = '1';
-        board[4][2] = '1';
-        board[3][1] = '1';
-        board[2][5] = '1';
+        board[5][0] = 1;
+        board[4][2] = 1;
+        board[3][1] = 1;
+        board[2][5] = 1;
+        console.log(board);
         expect(checkForWin()).toEqual(false);
+    })
+})
+describe('win()', () => {
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
+    it('should calculate a win for a horizontal set', () => {
+        board[5][0] = 1;
+        board[5][1] = 1;
+        board[5][2] = 1;
+        board[5][3] = 1;
+        test = [[5,0],[5,1],[5,2],[5,3]];
+        expect(win(test)).toEqual(true);
+    })
+
+    it('should calculate a win for a vertical set', () => {
+        board[5][0] = 1;
+        board[4][0] = 1;
+        board[3][0] = 1;
+        board[2][0] = 1;
+        test = [[5,0],[4,0],[3,0],[2,0]];
+        expect(win(test)).toEqual(true);
+    })
+
+    it('should calculate a win for a diagonal down-right set', () => {
+        board[0][0] = 1;
+        board[1][1] = 1;
+        board[2][2] = 1;
+        board[3][3] = 1;
+        test = [[0,0],[1,1],[2,2],[3,3]];
+        expect(win(test)).toEqual(true);
+    })
+
+    it('should calculate a win for a diagonal down-left set', () => {
+        board[0][3] = 1;
+        board[1][2] = 1;
+        board[2][1] = 1;
+        board[3][0] = 1;
+        test = [[0,3],[1,2],[2,1],[3,0]];
+        expect(win(test)).toEqual(true);
+    })
+
+    it('should not calculate a win for a horizontal set that doesnt all belong to the same person', () => {
+        board[5][0] = 2;
+        board[5][1] = 1;
+        board[5][2] = 1;
+        board[5][3] = 1;
+        currPlayer = 1;
+        test = [[5,0],[5,1],[5,2],[5,3]];
+        expect(win(test)).toEqual(false);
+    })
+
+    it('should not calculate a win for a vertical set that doesnt all belong to ther same person', () => {
+        board[5][0] = 1;
+        board[4][0] = 2;
+        board[3][0] = 1;
+        board[2][0] = 1;
+        currPlayer = 1;
+        test = [[5,0],[4,0],[3,0],[2,0]];
+        expect(win(test)).toEqual(false);
+        
+    })
+
+    it('should not calculate a win for a diagonal down-right set that doesnt all belong to ther same person', () => {
+        board[0][0] = 1;
+        board[1][1] = 1;
+        board[2][2] = 1;
+        board[3][3] = 2;
+        currPlayer = 2;
+        test = [[0,0],[1,1],[2,2],[3,3]];
+        expect(win(test)).toEqual(false);
+        
+    })
+
+    it('should not calculate a win for a diagonal down-left set that doesnt all belong to ther same person', () => {
+        board[0][3] = 1;
+        board[1][2] = 1;
+        board[2][1] = 2;
+        board[3][0] = 1;
+        currPlayer = 2;
+        test = [[0,3],[1,2],[2,1],[3,0]];
+        expect(win(test)).toEqual(false);
+    })
+
+    it('should not calculate a horizontal set out of range', () => {
+        test = [[HEIGHT-1, -1],[HEIGHT-1, 0],[HEIGHT-1, 1],[HEIGHT-1, 2]];
+        expect(win(test)).toEqual(false);
+    })
+
+    it('should not calculate a vertical set out of range', () => {
+        test = [[HEIGHT, 0],[HEIGHT-1, 0],[HEIGHT-2, 0],[HEIGHT-3, 0]];
+        expect(win(test)).toEqual(false);
+
+    })
+
+    it('should not calculate a diagonal down-right set out of range', () => {
+        test = [[HEIGHT, WIDTH],[HEIGHT-1, WIDTH-1],[HEIGHT-2, WIDTH-2],[HEIGHT-3, WIDTH-3]];
+        expect(win(test)).toEqual(false);
+    })
+
+    it('should not calculate a diagonal down-left set out of range', () => {
+        test = [[0,WIDTH],[1, WIDTH-1],[2,WIDTH-2],[3, WIDTH-3]]
+        expect(win(test)).toEqual(false);
     })
 })
 
 describe('checkForTie()', () => {
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
     it('should return false for a non-tie', () => {
-        
+        board.forEach(value => value.fill(1));
+        board[HEIGHT-1][WIDTH-1] = null;
+        expect(checkForTie()).toEqual(false);
     })
 
     it('should return true for a tie', () => {
-        
+        board.forEach(value => value.fill(1));
+        expect(checkForTie()).toEqual(true);
     })
 })
 
 describe('findSpotForCol()', () => {
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
     it('should return -1 for a full column', () => {
-
+        board.forEach(value => value[0] = 1);
+        expect(findSpotForCol(0)).toEqual(-1);
     })
 
     it('should return the correct y value for an non-full column', () => {
-
+        board.forEach(value => value[0] = 1);
+        board[0][0] = null;
+        expect(findSpotForCol(0)).toEqual(0);
     })
 })
 
 describe('placeInTable()', () => {
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
     it('should successfully place a div at the provided coordinates within range', () => {
-
+        makeHtmlBoard();
+        placeInTable(0,0);
+        expect(document.getElementById('0-0').hasChildNodes()).toEqual(true);
     })
 })
 
 describe('makeHtmlBoard', () => {
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
     it('should generate a board based on set height and width', () => {
-
+        HEIGHT = 5;
+        WIDTH = 3;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+        makeHtmlBoard();
+        const test = document.querySelectorAll('.cell')
+        expect(test.length).toEqual(15);
     })
 
     it('should generate a board even if the width and height are set to 1', () => {
-
+        HEIGHT = 1;
+        WIDTH = 1;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+        makeHtmlBoard();
+        const test = document.querySelectorAll('.cell')
+        expect(test.length).toEqual(1);
     })
 })
 
 describe('switchPlayer()', () => {
-    it('should be player 1 when the move count is any odd number over 0', () => {
-
+    afterEach(() => {
+        currPlayer = 1;
+        HEIGHT = 6;
+        WIDTH = 7;
+        moves = 0;
+        board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null)).map((x) => x.map(y => y));
+    })
+    it('should be player 2 when the move count is any odd number over 0', () => {
+        moves = 5;
+        switchPlayer();
+        expect(currPlayer).toEqual(2);
     })
 
-    it('should be player 2 when the move count is any positive number over 0', () => {
-
-    })
-
-    it('should start out at player 1', () =>{
-
+    it('should be player 1 when the move count is any positive number over 0', () => {
+        moves = 8;
+        switchPlayer();
+        expect(currPlayer).toEqual(1);
     })
 })
